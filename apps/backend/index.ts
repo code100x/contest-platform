c465a870620f8e759fd12397e6b5a3aa8267847c
harkirat <harkirat96@gmail.com>
2025-08-31 21:23:00 +0530

    Initial codebase

---
+++ apps/backend/index.ts
@@ -0,0 +1,10 @@
+
+import express from "express";
+
+const app = express();
+
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/contest", contestRouter);
+
+app.listen(process.env.PORT ?? 4000);
