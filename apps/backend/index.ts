6b9388e930af59020000036fbad2d29ea5b65c9e
harkirat <harkirat96@gmail.com>
2025-08-31 21:24:19 +0530

    Fix structure and imports

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,6 +1,8 @@
 
 import express from "express";
-
+import userRouter from "./routes/user"
+import contestRouter from "./routes/contest"
+import adminRouter from "./routes/admin"
 const app = express();
 
 app.use("/user", userRouter);
